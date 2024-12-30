19b6447b9b918a83c2b526f36990db85b68133df
LaSan99 <lasannavodya@gmail.com>
2024-12-30 10:35:45 +0530

    removed unwanted files

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  mongoURI:
-    process.env.MONGODB_URI ||
-    "mongodb+srv://lasannavodya:DelLCS5EiIBT3jFu@course.uf2pi.mongodb.net/Task-app?retryWrites=true&w=majority&appName=Course",
-  jwtSecret: process.env.JWT_SECRET || "wdbfiweufbiwebfwebf1243enlencewvw",
-};
-
-module.exports = config;
